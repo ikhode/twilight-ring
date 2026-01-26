7a65f01ced59b11d84f45719988dee2f00148ff6
Andrea Itzel <ikhode.com@gmail.com>
2026-01-25 22:00:51 -0600

    fix: Define __dirname and __filename in ESM bundle

--- script/build.ts
+++ script/build.ts
@@ -39,7 +39,14 @@ async function buildAll() {
     external: externals,
     logLevel: "info",
     banner: {
-      js: `import { createRequire } from 'module';const require = createRequire(import.meta.url);`,
+      js: `
+import { createRequire } from 'module';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+const require = createRequire(import.meta.url);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+`.trim(),
     },
   });
 }
