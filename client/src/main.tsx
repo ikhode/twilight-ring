7acc88d40ee12ac33ea83d9fb49bde80044946aa
Andrea Itzel <ikhode.com@gmail.com>
2026-01-26 18:56:20 -0600

    refactor: remove obsolete service worker, keep manifest for PWA terminals

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,17 +1,6 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
-
-// CLEANUP: Remove any stale service workers from previous deployments
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.getRegistrations().then((registrations) => {
-        for (const registration of registrations) {
-            console.log('Unregistering stale service worker:', registration);
-            registration.unregister();
-        }
-    });
-}
-
 import { GlobalErrorBoundary } from "./components/layout/GlobalErrorBoundary";
 
 createRoot(document.getElementById("root")!).render(
